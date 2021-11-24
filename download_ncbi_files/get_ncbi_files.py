d108ca5af08c2544556ed4dc52a0cc2565a9965f
Keffy <kehrli@gmail.com>
2021-11-24 15:43:16 -0500

    Cleaned up Python script for downloading genome and proteome files from NCBI for downstream use.

---
+++ download_ncbi_files/get_ncbi_files.py
@@ -0,0 +1,82 @@
+#!/bin/python
+
+### Use Entrez utilities through Biopython to get
+### the RefSeq genome and proteome urls and download the files.
+
+import argparse
+import wget
+from Bio import Entrez
+from time import sleep
+
+
+### Functions! Functions! Functions!
+### Get ncbi url
+
+def get_ncbi_ftp_path(orgn):
+    handle1 = Entrez.esearch(db="genome",term=orgn)
+    sleep(0.1)
+    records = Entrez.read(handle1)
+    sp_id = records['IdList']
+    handle2 = Entrez.esummary(db="genome",id=sp_id,retmax="100")
+    sleep(0.1)
+    records2 = Entrez.read(handle2)
+    accession = records2[0]["Assembly_Accession"]
+    handle3 = Entrez.esearch(db="assembly", term=accession, retmax="100")
+    sleep(0.1)
+    records3 = Entrez.read(handle3)
+    assembly_id = records3['IdList']
+    if (len(assembly_id) > 1):
+        full_summaries = []
+        for id in assembly_id:
+            handle = Entrez.esummary(db="assembly",id=id, report="full")
+            record = Entrez.read(handle)
+            full_summaries.append(record)
+        dates = [summary['DocumentSummarySet']['DocumentSummary'][0]['AsmReleaseDate_RefSeq'] for summary in full_summaries]
+        assembly_id = assembly_id[dates.index(max(dates))]
+    handle4 = Entrez.esummary(db="assembly",id=assembly_id, report="full")
+    sleep(0.1)
+    summary = Entrez.read(handle4)
+    ftp_path = summary['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq']
+    assembly_acc = summary['DocumentSummarySet']['DocumentSummary'][0]['AssemblyAccession']
+    handle1.close()
+    handle2.close()
+    handle3.close()
+    handle4.close()
+    sleep(1)
+    return(ftp_path,assembly_acc)
+
+
+def download_refseq(ftp_path, out_dir, species_lst):
+    for ftp in ftp_path:
+        current_sp = species_lst.index(ftp)
+        current_sp = current_sp.replace(" ", "_")
+        genome_ftp = ftp_path + "_genomic.fna.gz"
+        protein_ftp = ftp_path + "_protein.faa.gz"
+        #get output filename
+        genome_outfile = out_dir + "/" + current_sp + "_genomic.fna.gz"
+        protein_outfile = out_dir + "/" + current_sp + "_protein.faa.gz"
+        wget.download(genome_ftp, genome_outfile)
+        wget.download(protein_ftp, protein_outfile)
+
+
+### parse passed email argument
+
+parser.add_argument("--email", help="You need to set your email address for NCBI.", required=True)
+parser.add_argument("--api_key", help="Supply an API key to NCBI for more requests per second.")
+parser.add_argument("--out_dir", help="Supply an output directory for file downloads.")
+arg_dict = vars(parser.parse_args())
+Entrez.email = arg_dict['email']
+Entrez.api_key = arg_dict['api_key']
+output_dir = arg_dict['out_dir']
+
+### calls
+### Species list:
+
+with open("../meta/species_no_us.txt", "r") as fn:
+    species = fn.read().splitlines()
+
+
+ftp_accession_list = [get_ncbi_ftp_path(sp) for sp in species]
+ftp_paths, assembly_accessions = [i for i,j in ftp_accession_list]
+
+download_refseq(ftp_path, out_dir, species)
