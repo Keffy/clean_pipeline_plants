da8fe4c36059f8e27f7c2193a7703b1b488012a5
Keffy <kehrli@gmail.com>
2021-11-28 10:26:13 -0500

    Got the git code to call all of the SRR numbers from NCBI.

--- RNA_seq/pull_srr_nums.sh
+++ RNA_seq/pull_srr_nums.sh
@@ -5,3 +5,8 @@
 #esearch -db sra -query "SRX1821735" | efetch -format runinfo | egrep "SRR*,"
 
 #esearch -db sra -query "SRX1821735" | efetch -format runinfo | egrep -o "SRR[0-9]+" | head -n 1 
+
+# Read in all SRX numbers
+
+# search all and write to csv file.
+esearch -db sra -query [SRX] | efetch -format runinfo | tail -n +2 >> SRA_run_info.csv
