f8e34c8a1cd992f4d8e779823b02dc17586cab09
Keffy <kehrli@gmail.com>
2022-01-03 16:38:07 -0500

    IQ_tree scripts and GFF parsing.

---
+++ meta/parse_gff_files.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+## Parse the GFF files to link protein and RNA accession numbers
+
+import pprint
+from BCBio.GFF import GFFExaminer
+from BCBio import GFF
+
--- phylogeny/iq_tree_all_sp_no_identical_seqs.sh
+++ phylogeny/iq_tree_all_sp_no_identical_seqs.sh
@@ -11,11 +11,13 @@
 
 module load shared
 module load iq-tree/2.0.6
+module load anaconda/3
+source activate t_coffee
 
 mafft_dir="/gpfs/projects/RestGroup/keffy/scratch/mafft_alignments/"
-output_dir="/gpfs/projects/RestGroup/keffy/scratch/iq_tree_trim/"
+output_dir="/gpfs/projects/RestGroup/keffy/scratch/iq_tree_trim_noid/"
 meta_dir="/gpfs/projects/RestGroup/keffy/clean_pipeline_plants/meta/"
-summary_dir="/gpfs/projects/RestGroup/keffy/scratch/trim_summaries/"
+summary_dir="/gpfs/projects/RestGroup/keffy/scratch/trim_summaries_noid/"
 
 mapfile -t Orthogroups < ${meta_dir}OGs_all_sp.txt
 
@@ -23,13 +25,15 @@ for (( i=${#Orthogroups[@]}-1; i>=0; i--)); do
     rev[${#rev[@]}]=${Orthogroups[i]}
 done
 
-## trimming alignment to remove regions with more gaps in more than 50% of sequences, unless we will lose more than 60% of the alignment.
+## trimming alignment to remove based on the heuristic automated1 method.
 
 for OG in "${rev[@]}"; do
-    trimal -in ${mafft_dir}${OG}_aln.fa -out ${mafft_dir}${OG}_aln_trim.fa -fasta -htmlout ${summary_dir}${OG}_trimal_summary.html -gt 0.5 -cons 60 -w 3 
+    echo ${OG}
+    trimal -in ${mafft_dir}${OG}_aln.fa -out ${mafft_dir}${OG}_aln_trim_auto.fa -fasta -htmlout ${summary_dir}${OG}_trimal_summary.html -automated1
 done
 
 for OG in "${rev[@]}"; do
-    sed "s/ /_/g" ${mafft_dir}${OG}_aln_trim.fa > ${mafft_dir}${OG}_aln_us_trim.fa
-    iqtree2 -s ${mafft_dir}${OG}_aln_us_trim.fa -m MFP -T AUTO -ntmax 40 -pre ${output_dir}${OG}_iqtree --redo
+    echo ${OG}
+    sed "s/ /_/g" ${mafft_dir}${OG}_aln_trim_auto.fa > ${mafft_dir}${OG}_aln_us_trim_auto.fa
+    iqtree2 -s ${mafft_dir}${OG}_aln_us_trim_auto.fa -m MFP -T AUTO -ntmax 40 -pre ${output_dir}${OG}_iqtree --redo
 done
